d0417012670c99c129e9016257e50ea4f483b977
SEBASTIAN AGUILAR <SEBASTIAN AGUILAR@DESKTOP-I8IULA4>
2022-09-26 03:21:02 -0700

    Agregar archivos de proyecto.

---
+++ KyoruguiMDK1.0/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace KyoruguiMDK1._0
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void BtnRojo1_Clicked(object sender, EventArgs e)
+        {
+            Label_editar.Text = "BOTON ROJO 1 FUNCIONANDO";
+        }
+
+        private void btnRojo5_Clicked(object sender, EventArgs e)
+        {
+            Label_editar.Text = "BOTON ROJO 5 FUNCIONANDO";
+        }
+
+        private void BtnRojo2_Clicked(object sender, EventArgs e)
+        {
+            Label_editar.Text = "BOTON ROJO 2 FUNCIONANDO";
+
+        }
+
+        private void BtnRojo3_Clicked(object sender, EventArgs e)
+        {
+            Label_editar.Text = "BOTON ROJO 3 FUNCIONANDO";
+
+        }
+
+        private void BtnRojo4_Clicked(object sender, EventArgs e)
+        {
+            Label_editar.Text = "BOTON ROJO 4 FUNCIONANDO";
+
+        }
+
+        private void BtnAzul1_Clicked(object sender, EventArgs e)
+        {
+            Label_editar.Text = "BOTON AZUL 1 FUNCIONANDO";
+        }
+
+        private void BtnAzul2_Clicked(object sender, EventArgs e)
+        {
+            Label_editar.Text = "BOTON AZUL 2 FUNCIONANDO";
+        }
+
+        private void BtnAzul3_Clicked(object sender, EventArgs e)
+        {
+            Label_editar.Text = "BOTON AZUL 3 FUNCIONANDO";
+        }
+
+        private void BtnAzul4_Clicked(object sender, EventArgs e)
+        {
+            Label_editar.Text = "BOTON AZUL 4 FUNCIONANDO";
+        }
+
+        private void btnAzul5_Clicked(object sender, EventArgs e)
+        {
+            Label_editar.Text = "BOTON AZUL 5 FUNCIONANDO";
+        }
+    }
+    
+}
